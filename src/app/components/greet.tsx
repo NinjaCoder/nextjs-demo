31e6e283d2ebbfda454248fc3b28d29cbfb31328
Sohel Biswas <sohelbiswas23@gmail.com>
2025-06-06 10:17:37 +0530

    greet components

---
+++ src/app/components/greet.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Greet() {
+    console.log("Greet Component");
+  return (
+    <div>greeting from Greet component</div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+import  Greet  from "./components/greet"; 
+  export default function Home() {
   return (
     <div>
      <h1>If you see this,This means i am able to push code directly to vercel.</h1>
+     <Greet/>
     </div>
   );
 }
